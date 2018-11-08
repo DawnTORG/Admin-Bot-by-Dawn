a99991043ad8e096fa98edf95d132b9716facd31
DawnTORG <41655849+DawnTORG@users.noreply.github.com>
2018-11-08 15:44:28 +0100

    Update adminbot.js

--- adminbot.js
+++ adminbot.js
@@ -7,6 +7,10 @@ client.on('ready', () => {
 	console.log("Bot jetzt angeschaltet\n\n")
 });
 
+client.on('ready', () => {
+	console.log("Bot jetzt angeschaltet\n\n")
+});
+
 client.on('message', message => {
 	
 	if(message.author.bot) return;
@@ -17,22 +21,20 @@ client.on('message', message => {
 	   if (mention == null) { return; }
            message.delete();
 		   mentionMessage = message.content.slice (6);
-		   mention.send ('```css\nHey! Der User ```' + message.author + '```css\n braucht einen Admin. Hier ist das Problem: ```\n' + mentionMessage);
-		   message.author.send ('```css\nHallo! Deine Adminanfrage wird so schnell wie möglich bearbeitet. Viel Spaß noch auf dem Server!```' + 
-		   '```css\nDas ist das Problem welches du geschrieben hast:```\n' + mentionMessage);
-                   message.reply('```css\n' + 'Ich habe dir Infos zu deiner Adminanfrage Privat geschrieben.' + '```');
+	
+		   var embed = new Discord.RichEmbed()
+		       .addField('Hey! Der User ' + message.author + ' braucht einen Admin. Hier ist das Problem:', 'Hier ist das Problem: ' + mentionMessage)
+		       .setColor('RED')
+		   mention.sendEmbed(embed)
+		   
+		   var embed = new Discord.RichEmbed()
+		     .addField('Hallo! Deine Adminanfrage wird so schnell wie möglich bearbeitet. Viel Spaß noch auf dem Server!', 'Das ist das Problem welches du geschrieben hast: ' + mentionMessage)
+		     .setColor('RED')
+                   message.author.sendEmbed(embed)
+		   
 		   console.log("Es wurde folgende Meldung von dem Spieler " + message.author + " gesendet: " + mentionMessage + " \n Die meldung ging an den Admin " + mention)
 		   
 	}
-		   //!help 
-    else if(message.content.toLowerCase() === prefix + 'help'){
-            message.delete();
-	    message.author.send('```css\n' + 
-       	    'Du kannst einen Admin über ein Problem das du hast informieren durch das eingeben von: !admin (der Gewünschte Admin) (der Grund)\nAussehen muss das zum Beispiel so: !admin @Dawn Test'		
-	    + '```');
-								   console.log("\n\nEs wurde die Help-liste Aufgerufen")
-	}
-
 	
 });
 
